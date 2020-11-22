5b71f87a18ed3fc305fa0cc8272961987aaca0a8
Mujib <mujib517@gmail.com>
2020-11-22 13:32:30 +0530

    Add tests for the controllers

---
+++ src/test/java/com/aws/api/demo/controllers/HomeControllerTests.java
@@ -0,0 +1,22 @@
+package com.aws.api.demo.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class HomeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn200Status() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+}
